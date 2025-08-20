43f2eb981e4eba6199b9205953938ade12309fa1
ThanSoe <148028841+thansoeoo-lastoenjoy@users.noreply.github.com>
2025-08-21 04:54:18 +0700

    static params & static prerendering

---
+++ src/app/products/page.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function ProductsPage() {
+  return (
+    <div>
+      <Link href="/products/1">Product 1</Link>
+      <Link href="/products/2">Product 2</Link>
+      <Link href="/products/3">Product 3</Link>
+    </div>
+  );
+}
