f6c4f84b9e33a0fecfafa99427dafd189d53e14e
ThanSoe <148028841+thansoeoo-lastoenjoy@users.noreply.github.com>
2025-08-21 05:52:29 +0700

    context providers

--- src/app/client-route/page.tsx
+++ src/app/client-route/page.tsx
@@ -1,10 +1,13 @@
 "use client";
-import { serverSideFunction } from "@/utils/server-utils";
+
+import { useTheme } from "@/components/theme-provider";
+
 export default async function ClientRoutePage() {
-  const result = serverSideFunction();
+  const theme = useTheme();
+
   return (
     <div>
-      <h1>Client Route {result}</h1>
+      <h1 style={{ color: theme.colors.secondary }}>Client Route</h1>
     </div>
   );
 }
--- src/utils/server-utils.ts
+++ src/utils/server-utils.ts
@@ -1,4 +1,3 @@
-import "server-only";
 export const serverSideFunction = () => {
   console.log(`use multiple libraries,
         user environment variables,
